590793be0b8965794ecd075ecc9cf2eedb269a15
Muqaddas <muqaddasnigar@gmail.com>
2026-02-16 07:59:55 +0500

    refactor cleaner.py

--- cleaning/cleaner.py
+++ cleaning/cleaner.py
@@ -3,12 +3,6 @@ import logging
 import re
 
 
-
-def drop_duplicates(df, validator=None):
-    dup_count = df.duplicated().sum()
-    df.drop_duplicates(inplace=True)
-
-
 def convert_to_object(col_series):
     logging.info(f'Converting "{col_series.name}" to object')
     converted_col = col_series.astype('object')
@@ -30,58 +24,16 @@ def convert_to_numeric(col_series):
 def convert_dtypes(df, col_dtype_map):
     logging.info('---Converting Dtypes---')
     for col, col_type in col_dtype_map.items():
-        if col_type.lower() == 'numeric':
+        if col_type.lower() == 'numerics':
             df[col] = convert_to_numeric(df[col])
-        elif col_type.lower() == 'date':
+        elif col_type.lower() == 'dates':
             df[col] = convert_to_dates(df[col])
         else:
             df[col] = convert_to_object(df[col])
     return df
 
 
-def is_date(sample):
-    garbage_count = 0
-    empty_count = 0
-    re_sample = []
-    date_regex = r'(?i)(\d{1,4}[-./]\d{1,2}[-./]\d{1,4}|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)'
-    for value in sample:
-        v_str = str(value).lower().strip()
-        if v_str in ['nan', 'na', 'n/a', 'null', '', 'none', '<na>']:
-            empty_count += 1
-            continue
-        try:
-            float_val = float(value)
-            if float_val > 3000:
-                 pass
-            else:
-                garbage_count += 1
-                continue
-        except (ValueError, TypeError):
-            pass
-        if not re.search(date_regex, v_str):
-            garbage_count += 1
-            continue
-        try:
-            pd.to_datetime(value, format='mixed', errors='raise', dayfirst=True)
-            re_sample.append(value)
-        except (ValueError, TypeError, OverflowError):
-            if re.search(date_regex, v_str):
-                re_sample.append(value)
-            else:
-                garbage_count += 1
-    clean_sample = pd.Series(re_sample)
-    if clean_sample.empty:
-        return 0
-    converted = pd.to_datetime(clean_sample, format='mixed', errors='coerce', dayfirst=True)
-    passed_count = converted.notna().sum()
-    if (empty_count + garbage_count) / len(sample) >= 0.5:
-        return 0
-    else:
-        total_values = len(sample) - empty_count
-        return passed_count / total_values if total_values > 0 else 0
-
-
-def clean_numerics(col_series, validator=None):
+def clean_numerics(col_series):
     if isinstance(col_series, list):
         col_series = pd.Series(col_series)
     clean_s = col_series.astype(str).str.strip().str.replace(r'^\((.*?)\)$', r'-\1', regex=True)
@@ -90,7 +42,20 @@ def clean_numerics(col_series, validator=None):
     return col_series
 
 
-def is_accounting_numeric(value):
+def assign_dtype(total, empty, numerics, dates, threshold):
+    non_empty = total - empty
+    if non_empty == 0:
+        return 'object'
+    if empty/total >= threshold:
+        return 'object'
+    if numerics/non_empty >= threshold:
+        return 'numerics'
+    if dates/non_empty >= threshold:
+        return 'dates'
+    return 'object'
+
+
+def accounting_numeric(value):
     s = str(value).strip()
     is_negative = s.count('(') > 0 and s.count(')') > 0
     cleaned = re.sub(r'[(),\s]', '', s)
@@ -101,75 +66,71 @@ def is_accounting_numeric(value):
     except (ValueError, TypeError):
         return None
 
-def is_numeric(sample):
-    garbage_count = 0
-    empty_count = 0
-    re_sample = []
-    date_regex = r'(?i)(\d{2}[-./]\d{2}[-./]\d{4}|\d{4}[-./]\d{2}[-./]\d{2}|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)'
-    for value in sample:
-        if str(value).lower().strip() in ['nan', 'na', 'n/a', 'null', '', 'none', '<na>']:
-            empty_count += 1
-            continue
-        try:
-            if isinstance(float(value), float):
-                re_sample.append(value)
-                continue
-        except (ValueError, TypeError):
-            pass
-        if not re.search(r'\d', str(value)):
-            garbage_count += 1
-            continue
-        try:
-            pd.to_datetime(value, format='mixed', errors='raise', dayfirst=True)
-            garbage_count += 1
-        except ValueError:
-                pass
+
+def if_acc_numeric(value):
+    value = accounting_numeric(value)
+    if value is not None:
+        return 1
+    return 0
+
+
+def if_numeric(value):
+    try:
+        float(value)
+        return 1
+    except (ValueError, TypeError):
+        return 0
+
+
+def if_date(value):
+    date_regex = r'(?i)(\d{1,4}[-./]\d{1,2}[-./]\d{1,4}|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)'
+    try:
+        pd.to_datetime(value, format='mixed', dayfirst=True, errors='raise')
+        return 1
+    except Exception:
         if re.search(date_regex, str(value)):
-            garbage_count += 1
-            continue
-        if is_accounting_numeric(value):
-            re_sample.append(is_accounting_numeric(value))
-            continue
-        re_sample.append(value)
-    clean_sample = pd.Series(re_sample)
-    if clean_sample.empty:
+            return 1
         return 0
-    converted = pd.to_numeric(clean_sample, errors='coerce')
-    passed_count = converted.notna().sum()
-    if (empty_count + garbage_count) / len(sample) >= 0.5:
-        return 0
-    else:
-        total_values = len(sample) - empty_count
-        return passed_count/total_values if total_values > 0 else 0
 
 
-def detect_dtypes(col_series, threshold):
-    if threshold >= 0.95:
-        gray_zone = threshold - 0.05
-    elif threshold >= 0.80:
-        gray_zone = threshold - 0.10
-    else:
-        gray_zone = threshold * 0.7
-    result = is_numeric(col_series)
-    if result >= threshold:
-        return "numeric"
-    if result >= gray_zone:
-        old_result = result
-        clean_sample = clean_numerics(col_series)
-        new_result = is_numeric(clean_sample)
-        if new_result > old_result:
-            if new_result >= threshold or threshold - new_result < 0.02:
-                return 'numeric'
-    result = is_date(col_series)
-    if result >= threshold:
-        return 'date'
-    return 'object'
+def if_empty(value):
+    if str(value).lower().strip() in ['nan', 'na', 'n/a', 'null', '', 'none', '<na>']:
+        return 1
+    return 0
+
+
+def identify_dtypes(col_series, threshold):
+    total = col_series.shape[0]
+    empty = 0
+    numerics = 0
+    accounting_numerics = 0
+    dates = 0
+    for value in col_series:
+        is_empty = if_empty(value)
+        if is_empty:
+            empty += 1
+            continue
+        is_date = if_date(value)
+        if is_date:
+            dates += 1
+            continue
+        is_numeric = if_numeric(value)
+        if is_numeric:
+            numerics += 1
+            continue
+        is_acc_numeric = if_acc_numeric(value)
+        if is_acc_numeric:
+            accounting_numerics += 1
+            continue
+    numerics += accounting_numerics
+    dtype = assign_dtype(total, empty, numerics, dates, threshold)
+    return dtype
 
 
 def assign_dtypes(df_sample, threshold):
     col_dtype_map = {}
     for col in df_sample.columns:
-        d_type = detect_dtypes(df_sample[col], threshold)
+        d_type = identify_dtypes(df_sample[col], threshold)
         col_dtype_map[col] = d_type
     return col_dtype_map
 
@@ -179,8 +140,9 @@ def take_sample(df, sample_size):
     target_sample = int(total_rows * sample_size)
     min_sample = 10 if 10 <= df.shape[0] else df.shape[0]
     sample = max(min(target_sample, total_rows), min_sample)
-    df_sample = df.sample(n=sample).copy()
-    if df_sample is None:
+    try:
+        df_sample = df.sample(n=sample).copy()
+    except ValueError:
         return None
     else:
         return df_sample
@@ -189,12 +151,16 @@ def take_sample(df, sample_size):
 def map_dtypes(df, tuning):
     sample_size = tuning[0]
     threshold = tuning[1]
+    logging.info('Taking data sample for dtype mapping.')
     df_sample = take_sample(df, sample_size)
+    if df_sample is None:
+        logging.error('Sampling failed.')
+        raise ValueError
     col_dtype_map = assign_dtypes(df_sample, threshold)
     return col_dtype_map
 
 
-def normalize_headers(df, validator=None):
+def normalize_headers(df):
     logging.info('---Normalizing Headers---')
     try:
         normalized_cols = df.columns.str.strip().str.lower().str.replace(' ', '_')
@@ -214,22 +180,15 @@ def normalize_headers(df, validator=None):
     return df
 
 
-def cleaner(data, tuning, validator=None):
+def cleaner(data, tuning):
     df = data.copy()
     logging.info('----Cleaning Pipeline----')
     try:
-        df = normalize_headers(df, validator)
+        df = normalize_headers(df)
     except AttributeError:
         logging.critical(f'Expected DataFrame received "{type(df)}"')
+        raise AttributeError
     else:
         column_dtype_map = map_dtypes(df, tuning)
         df = convert_dtypes(df, column_dtype_map)
-        drop_duplicates(df, validator)
-    return df, validator
-
-def clean_data(data):
-    sample_size = 0.1
-    threshold = 0.9
-    tuning = (sample_size, threshold)
-    data, report = cleaner(data, tuning, None)
-    return data
+    return df
